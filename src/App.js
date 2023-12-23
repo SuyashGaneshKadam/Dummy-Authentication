e0c8dd2ba643aeae36de4deaee565793b65131ba
SuyashGaneshKadam <9044suyash@gmail.com>
2023-12-23 20:58:46 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function handleSubmit(e){
+    e.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <p id="welcome">Welcome back!👋</p>
+        <h2>Sign up to your account</h2>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <div className="password">
+          <input
+            id="password"
+            className="password-input"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="submit">CONTINUE</button>
+        <p id="forgot-password">Forgot your password?</p>
+        <p id="error">Error</p>
+      </form>
+      <p id="sign-up-button">
+        Don't have an account? <a href="#">Sign up</a>
+      </p>
     </div>
   );
-}
+};
 
 export default App;
